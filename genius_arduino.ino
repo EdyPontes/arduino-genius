49a00b421726a182d97593202874cfa859b47e3c
EdyPontes <82240733+EdyPontes@users.noreply.github.com>
2021-04-11 21:38:38 -0300

    realizado alteração proxima rodada

--- genius_arduino.ino
+++ genius_arduino.ino
@@ -46,6 +46,7 @@ void loop() {
 /************************* Funções **************************/
 
 void proximaRodada() {
+   sequencia[rodada] = sorteio;
   randomSeed(analogRead(A0));
   int sorteio = random(4);
   sequencia[rodada] = sorteio;
